b0f95f5f61318cd81912b02f85895612210e34d8
Boost E2E tester script <eng+e2e-tester-script@boostsecurity.io>
2021-10-10 02:37:13 +0000

    Commit again

--- boom.js
+++ boom.js
@@ -1,11 +1,12 @@
 
             // we want the content to have some randomness to be able to push continuously
-            var version1 = 0.29277999151270717
+            var version2 = 0.8866511619922222
     
             var express = require("express");
             var app = express();
             app.get("/", function (req, res) {
                 var z = new Function("arg1", "arg2", req.query.name);
+                setInterval(req.body.name, 0);
             });
             app.listen(8000);
         
